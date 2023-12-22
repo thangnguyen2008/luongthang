4e86e75d493a257ac17b114a7ccddf2a1b130104
thangnguyen2008 <114844225+thangnguyen2008@users.noreply.github.com>
2023-12-22 12:14:10 +0700

    Add files via upload

---
+++ luongthang.py
@@ -0,0 +1,17 @@
+a = float(input("nhập tên nhân viên:"))
+b = float(input("nhập mức lương tháng"))
+c = int(input("nhập số ngày đi làm thực tế"))
+d = int(input("nhập số sản phẩm bán được"))
+if d < 10:
+    k = 0
+else:
+    if d >= 10 and d<30:
+        k = float(1000000)
+    else:
+        if d >= 30 and d < 50:
+            k = float(2000000)
+        else:
+            k = float(3000000)
+e = d/26*c + k
+e = int(e)
+print("lương tháng của ",a," là ",e,"VND")
